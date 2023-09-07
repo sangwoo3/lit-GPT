c1f20c4d797fa101469e2334cbfc7390f995e6ba
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-06 18:54:12 -0700

    no validation

--- run_scripts/run_pretrain.sh
+++ run_scripts/run_pretrain.sh
@@ -2,4 +2,4 @@ python pretrain/retnet_trainer_fabric.py \
 --devices 1 \
 --exp_name retnet_3b_redpajama_sample \
 --train_data_dir data/lit-redpajama-sample \
---val_data_dir data/lit-redpajama-sample-val \
+#--val_data_dir data/lit-redpajama-sample-val \

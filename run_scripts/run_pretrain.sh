aca42330c85454f999a950f32107d9496c580acf
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-06 19:08:53 -0700

    device=8

--- run_scripts/run_pretrain.sh
+++ run_scripts/run_pretrain.sh
@@ -1,5 +1,5 @@
 python pretrain/retnet_trainer_fabric.py \
---devices 1 \
+--devices 8 \
 --exp_name retnet_3b_redpajama_sample \
 --train_data_dir data/lit-redpajama-sample \
 #--val_data_dir data/lit-redpajama-sample-val \

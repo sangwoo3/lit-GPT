90b45524ce99cf9df14fd7ea7c2f971eb9c3cf78
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-14 15:39:14 -0700

    fix bug

--- jizhi/pretrain_retnet.sh
+++ jizhi/pretrain_retnet.sh
@@ -82,7 +82,7 @@ ALL_ARGS="${TRAIN_ARGS} \
 
 echo ${ALL_ARGS}
 
-CMD="python3 ${CODE_DIR}/pretrain/retnet_trainer_fabric.py ${ALL_ARGS}"
+CMD="python3 ${CODE_DIR}/pretrain/retnet_trainer_fabric_dev.py ${ALL_ARGS}"
 echo $CMD
 
 eval ${CMD} 2>&1 | tee -a ${OUTPUT_DIR}/${EXP_NAME}/log_node_${INDEX}.txt

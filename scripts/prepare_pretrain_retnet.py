761f51e202f2ac555b20c4fb6cdb9e7a0cadcf00
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-18 12:53:47 -0700

    preprocess retnet pretrain data

--- scripts/prepare_pretrain_retnet.py
+++ scripts/prepare_pretrain_retnet.py
@@ -1,11 +1,11 @@
 # import glob
 # import json
-# import os
+import os
 import sys
 from pathlib import Path
 
 import numpy as np
-# from tqdm import tqdm
+from tqdm import tqdm
 
 from functools import partial
 from transformers import AutoTokenizer
@@ -21,8 +21,8 @@ import lit_gpt.packed_dataset as packed_dataset
 
 tokenizer_dir = "/apdcephfs/share_300000800/user/swcho/huggingface_models/"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
-bos_id = tokenizer.bos_id
-eos_id = tokenizer.eos_id
+bos_id = tokenizer.bos_token_id
+eos_id = tokenizer.eos_token_id
 
 
 def process_data(data, tokenizer, bos=False, eos=False):
@@ -35,24 +35,23 @@ def process_data(data, tokenizer, bos=False, eos=False):
         if eos_id is None:
             raise NotImplementedError("This tokenizer does not defined a eos token")
         input_ids = input_ids + [eos_id]
-    # data["input_ids"] = input_ids
-    return input_ids
+    data["input_ids"] = input_ids
+    return data
 
 
-def process(source_path: Path,
+def process(source_file: str,
             destination_path: Path,
             prefix: str,
             chunk_size: int,
             num_proc: int):
-    source_file = str(source_path / "merged_360G_v2.jsonl")
-
     merged_dataset_streamed = load_dataset(
             "json", data_files=source_file, streaming=True
     )
     print(next(iter(merged_dataset_streamed)))
 
     process_dataset = partial(process_data, tokenizer=tokenizer, bos=True)
-    tokenized_dataset = merged_dataset_streamed.map(process_dataset, num_proc=num_proc)
+    tokenized_dataset = merged_dataset_streamed.map(process_dataset, num_proc=num_proc, batched=True)
+    tokenized_dataset = tokenized_dataset.rename_column("text")
     print(next(iter(tokenized_dataset)))
 
     destination_path.mkdir(parents=True, exist_ok=True)
@@ -67,7 +66,7 @@ def process(source_path: Path,
     )
 
     # TODO: test with small data
-    for td in tokenized_dataset:
+    for td in tqdm(tokenized_dataset):
         builder.add_array(np.array(td, dtype=builder.dtype))
 
     builder.write_reminder()
@@ -77,23 +76,25 @@ def prepare(
         source_path: Path = Path("/apdcephfs/share_300000800/user/riversong/pile/train"),
         destination_path: Path = Path("/apdcephfs/share_300000800/user/swcho/data/pretrain"),
         block_size: int = 2048,
-        prefix: str = "PCS-merged",
+        prefix: str = "PCS-merged-360G",
 ) -> None:
-    block_size = block_size
+    source_file = str(source_path / "merged_360G_v2.jsonl")
+    file_size_byte = os.stat(source_file).st_size
+    num_files = 512
+    chunk_size = file_size_byte // num_files // (block_size + 1)    # block size + 1 for causal
 
     max_cpus = multiprocessing.cpu_count()
     num_proc = int(max_cpus * 0.9)
 
     process(
-            source_path=source_path,
+            source_file=source_file,
             destination_path=destination_path,
             prefix=prefix,
-            chunk_size=(block_size + 1) * 1024 * 1024,  # block size + 1 for causal, 1024 * 1024 blocks
+            chunk_size=chunk_size,
             num_proc=num_proc,
     )
 
 
 if __name__ == "__main__":
     from jsonargparse import CLI
-
     CLI(prepare)

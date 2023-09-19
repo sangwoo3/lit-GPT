aa9d797b742ec0d8751f3c8450e00f9dc885c9bc
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-19 15:09:11 -0700

    data processing for pretrain: 0.01% of train for valid

--- scripts/prepare_pretrain_retnet.py
+++ scripts/prepare_pretrain_retnet.py
@@ -1,7 +1,8 @@
 # import glob
 # import json
-import os
+# import os
 import sys
+import time
 from pathlib import Path
 
 import numpy as np
@@ -23,6 +24,10 @@ tokenizer_dir = "/apdcephfs/share_300000800/user/swcho/huggingface_models/"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
 bos_id = tokenizer.bos_token_id
 eos_id = tokenizer.eos_token_id
+
+n_train_files = 512
+n_valid_files = 32
+block_size = 2048
 
 
 def process_data(data, tokenizer, bos=False, eos=False):
@@ -60,62 +65,65 @@ def build_packed_data(destination_path, prefix, chunk_size, dataset):
     print("[finished writing to files]")
 
 
-def process(source_file: str,
+def process(source_path: Path,
             destination_path: Path,
             prefix: str,
-            chunk_size: int,
             num_proc: int):
+    source_file = str(source_path / "merged_360G_v2.jsonl")
     merged_dataset_streamed = load_dataset(
             "json", data_files=source_file, streaming=True
     )
     print(next(iter(merged_dataset_streamed)))
 
+    t0 = time.time()
     process_dataset = partial(process_data, tokenizer=tokenizer, bos=True)
     tokenized_dataset = merged_dataset_streamed.map(process_dataset, remove_columns=["text"])
+    t1 = time.time()
     print(next(iter(tokenized_dataset)))
-    print("[finished tokenize]")
+    print(f"[finished tokenize] elapsed: {(t1-t0)*1000}sec")
 
-    n_tk = 0
+    n_ds, n_tk = 0, 0
     for tk in tokenized_dataset:
-        n_tk += 1
+        n_ds += 1
+        n_tk += len(tk['input_ids'])
+    print(f"total tokenized tokens: {n_tk}, dataset size: {n_ds}")
 
-    shuffled_tk_dataset = tokenized_dataset.shuffle(buffer_size=n_tk + 1, seed=42)
-    valid_count = int(n_tk * 0.0005)
+    # file_size_byte = os.stat(source_file).st_size
+    # chunk_size = file_size_byte // num_files // (block_size + 1)  # block size + 1 for causal
+    # print(f"source file size: {file_size_byte}bytes, target file count: {num_files}, chunk size: {chunk_size}")
+
+    shuffled_tk_dataset = tokenized_dataset.shuffle(buffer_size=n_ds + 1, seed=42)
+    n_val = int(n_ds * 0.0001)
     print("suffling is done with tokenized dataset")
-    print(f"valid instance / total instance: {valid_count} / {n_tk}")
-    shuffled_ds_train = shuffled_tk_dataset.skip(valid_count)
-    shuffled_ds_valid = shuffled_tk_dataset.take(valid_count)
+    print(f"valid instance: {n_val} / total instance: {n_ds}")
+    shuffled_ds_train = shuffled_tk_dataset.skip(n_val)
+    shuffled_ds_valid = shuffled_tk_dataset.take(n_val)
+
+    chunk_size_train = (n_ds - n_val) // n_train_files // (block_size + 1) * (block_size + 1)
+    chunk_size_valid = n_val // n_valid_files // (block_size + 1) * (block_size + 1)
 
     destination_path.mkdir(parents=True, exist_ok=True)
 
-    build_packed_data(destination_path, prefix+'-train',
-                      chunk_size, shuffled_ds_train)
+    build_packed_data(destination_path, prefix + '-train',
+                      chunk_size_train, shuffled_ds_train)
 
     build_packed_data(destination_path, prefix + '-val',
-                      chunk_size, shuffled_ds_valid)
+                      chunk_size_valid, shuffled_ds_valid)
 
 
 def prepare(
         source_path: Path = Path("/apdcephfs/share_300000800/user/riversong/pile/train"),
-        destination_path: Path = Path("/apdcephfs/share_300000800/user/swcho/data/pretrain"),
-        block_size: int = 2048,
+        destination_path: Path = Path("/apdcephfs/share_300000800/user/swcho/data/pretrain/retnet"),
         prefix: str = "PCS-merged-360G",
 ) -> None:
-    source_file = str(source_path / "merged_360G_v2.jsonl")
-    file_size_byte = os.stat(source_file).st_size
-    num_files = 512
-    chunk_size = file_size_byte // num_files // (block_size + 1)    # block size + 1 for causal
-    print(f"source file size: {file_size_byte}bytes, target file count: {num_files}, chunk size: {chunk_size}")
-
     max_cpus = multiprocessing.cpu_count()
-    num_proc = int(max_cpus * 0.9)
+    num_proc = int(max_cpus * 0.8)
     print(f"total cpu count: {max_cpus}, number of cpus to use: {num_proc}")
 
     process(
-            source_file=source_file,
+            source_file=source_path,
             destination_path=destination_path,
             prefix=prefix,
-            chunk_size=chunk_size,
             num_proc=num_proc,
     )
 

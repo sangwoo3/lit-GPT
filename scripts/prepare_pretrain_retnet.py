0bc78bc73f6587281185d67aa4c03c8492e48f80
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-21 10:58:14 -0700

    fix bug: no batch for doc, list exception

--- scripts/prepare_pretrain_retnet.py
+++ scripts/prepare_pretrain_retnet.py
@@ -31,7 +31,7 @@ else:
     eos_id = tokenizer.eos_id
 
 n_train_files = 512
-n_valid_files = 32
+n_valid_files = 64
 block_size = 2048
 n_tk_train_est = 100000000000
 n_tk_valid_est = 10000000
@@ -62,7 +62,8 @@ def split_data(data):
     else:
         text = data["text"]
     sentences = splitter.tokenize(text)
-    return {'sentences': sentences}
+    data['sentences'] = sentences
+    return data
 
 
 def process_data(data, tokenizer, bos=False, eos=False):
@@ -80,7 +81,8 @@ def process_data(data, tokenizer, bos=False, eos=False):
         if eos_id is None:
             raise NotImplementedError("This tokenizer does not defined a eos token")
         doc_ids = doc_ids + [eos_id]
-    return {"input_ids": doc_ids}
+    data["input_ids"] = doc_ids
+    return data
 
 
 def build_packed_data(destination_path, prefix, chunk_size, dataset):
@@ -159,36 +161,35 @@ def process(source_path: Path,
     # HF tokenize
     t0 = time.time()
     merged_dataset = merged_dataset.map(split_data,
-                                        remove_columns=["text"],
+                                        remove_columns=merged_dataset['train'].column_names,  # ["text"],
                                         num_proc=num_proc,
-                                        batched=True,
+                                        # batched=True,
                                         desc='Splitting...')
     t1 = time.time()
     print(f"[finished splitting] elapsed: {(t1 - t0) * 1000}sec")
 
     t0 = time.time()
     process_dataset = partial(process_data, tokenizer=tokenizer, bos=True)
-    tokenized_dataset = merged_dataset.map(process_dataset,
-                                           remove_columns=["sentences"],
-                                           num_proc=num_proc,
-                                           batched=True,
-                                           desc='Tokenizing...')
+    merged_dataset = merged_dataset.map(process_dataset,
+                                        remove_columns=["sentences"],
+                                        num_proc=num_proc,
+                                        # batched=True,
+                                        desc='Tokenizing...')
     t1 = time.time()
-    # print(next(iter(tokenized_dataset)))
-    print(tokenized_dataset[0])
+    # print(next(iter(merged_dataset)))
     print(f"[finished tokenize] elapsed: {(t1 - t0) * 1000}sec")
-    print(f"train dataset size: {len(tokenized_dataset['train'])}")
-    print(f"valid dataset size: {len(tokenized_dataset['valid'])}")
+    print(merged_dataset['train'][100])
 
     n_tk_train, n_tk_valid = 0, 0
-    for tk in tqdm(tokenized_dataset['train'], desc='train size...'):
+    for tk in tqdm(merged_dataset['train'], desc='train size...'):
         n_tk_train += len(tk['input_ids'])
-    for tk in tqdm(tokenized_dataset['valid'], desc='valid size...'):
+    for tk in tqdm(merged_dataset['valid'], desc='valid size...'):
         n_tk_valid += len(tk['input_ids'])
-    print(f"total token count: train-{n_tk_train}, valid-{n_tk_valid}")
+    print(f"[train] dataset size: {len(merged_dataset['train'])} / total token count: {n_tk_train}")
+    print(f"[valid] dataset size: {len(merged_dataset['valid'])} / total token count: {n_tk_valid}")
 
     # n_ds, n_tk = 0, 0
-    # for tk in tokenized_dataset:
+    # for tk in merged_dataset:
     #     n_ds += 1
     #     n_tk += len(tk['input_ids'])
     # print(f"total tokenized tokens: {n_tk}, dataset size: {n_ds}")
@@ -197,7 +198,7 @@ def process(source_path: Path,
     # chunk_size = file_size_byte // num_files // (block_size + 1)  # block size + 1 for causal
     # print(f"source file size: {file_size_byte}bytes, target file count: {num_files}, chunk size: {chunk_size}")
 
-    # shuffled_tk_dataset = tokenized_dataset.shuffle(buffer_size=n_ds_train + 1, seed=42)
+    # shuffled_tk_dataset = merged_dataset.shuffle(buffer_size=n_ds_train + 1, seed=42)
     # n_val = int(n_ds * 0.0001)
     # print("suffling is done with tokenized dataset")
     # print(f"valid instance: {n_val} / total instance: {n_ds}")
@@ -209,12 +210,16 @@ def process(source_path: Path,
     print(f"chunk size: train-{chunk_size_train}, valid-{chunk_size_valid}")
 
     destination_path.mkdir(parents=True, exist_ok=True)
+    dest_tokenized_path = destination_path / 'tokenized'
+    dest_tokenized_path.mkdir(parents=True, exist_ok=True)
+    merged_dataset.save_to_disk(dest_tokenized_path)
+    print(f"huggingface dataset is saved: {str(dest_tokenized_path)}")      # in case of error on packed data
 
     build_packed_data(destination_path, prefix + '-train',
-                      chunk_size_train, tokenized_dataset['train'])
+                      chunk_size_train, merged_dataset['train'])
 
     build_packed_data(destination_path, prefix + '-val',
-                      chunk_size_valid, tokenized_dataset['valid'])
+                      chunk_size_valid, merged_dataset['valid'])
 
 
 def prepare(

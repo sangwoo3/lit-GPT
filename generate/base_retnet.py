05c5aa8ee4ae412e766e922971869c40dbf05902
Sangwoo Cho <sangwoo3.cho@gmail.com>
2023-09-28 11:51:53 -0700

    generate

--- generate/base_retnet.py
+++ generate/base_retnet.py
@@ -89,6 +89,29 @@ def generate(
     return idx
 
 
+@torch.no_grad()
+def generate_(model, idx, max_new_tokens, block_size, temperature=1.0, top_k=None, incremental_state=None):
+    for _ in range(max_new_tokens):
+        # if the sequence context is growing too long we must crop it at block_size
+        idx_cond = idx if idx.size(1) <= block_size else idx[:, -block_size:]
+        # forward the model to get the logits for the index in the sequence
+        logits, _ = model(idx_cond, incremental_state=incremental_state)
+        # pluck the logits at the final step and scale by desired temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop the logits to only the top k options
+        if top_k is not None:
+            v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+            logits[logits < v[:, [-1]]] = -float('Inf')
+        # apply softmax to convert logits to (normalized) probabilities
+        probs = torch.nn.functional.softmax(logits, dim=-1)
+        # sample from the distribution
+        idx_next = torch.multinomial(probs, num_samples=1)
+        # append sampled index to the running sequence and continue
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+
 def main(args) -> None:
     precision = args.precision or get_default_supported_precision(training=False)
 
@@ -155,15 +178,23 @@ def main(args) -> None:
     L.seed_everything(1234)
     for i in range(args.num_samples):
         t0 = time.perf_counter()
-        y = generate(
-                model,
-                encoded,
-                max_returned_tokens,
-                max_seq_length=max_returned_tokens,
-                temperature=args.temperature,
-                top_k=args.top_k,
-                incremental_state=args.incremental_state,
-        )
+        # y = generate(
+        #         model,
+        #         encoded,
+        #         max_returned_tokens,
+        #         max_seq_length=max_returned_tokens,
+        #         temperature=args.temperature,
+        #         top_k=args.top_k,
+        #         incremental_state=args.incremental_state,
+        # )
+        model, idx, max_new_tokens, block_size, temperature = 1.0, top_k = None, incremental_state = None
+        y  = generate_(model,
+                       encoded,
+                       max_returned_tokens,
+                       model.config.block_size,
+                       temperature=args.temperature,
+                       top_k=args.top_k,
+                       incremental_state=args.incremental_state)
         t = time.perf_counter() - t0
 
         model.reset_cache()
@@ -186,7 +217,7 @@ def add_inference_args(parser):
                         help="The number of text samples to generate.")
     parser.add_argument("--max_new_tokens", type=int, default=50,
                         help="The number of generation steps to take.")
-    parser.add_argument("--top_k", type=int, default=200,
+    parser.add_argument("--top_k", type=int,
                         help="The number of top most probable tokens to consider in the sampling process.")
     parser.add_argument("--temperature", type=float, default=1.0,
                         help="A value controlling the randomness of the sampling process. "
